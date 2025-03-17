dd123586f51f8a8000a7c6f06bd5bc9fbe6cbe63
Prathoshini _Devi <137100216+Prathoshini07@users.noreply.github.com>
2025-03-17 08:53:01 +0530

    Update app.py

--- Project4/app.py
+++ Project4/app.py
@@ -8,16 +8,10 @@ app=Flask(__name__)
 def home():
     return render_template("index.html")
 
-<<<<<<< HEAD
-@app.route('/tickers.html')
-def getTicker():
-    url = "https://fresh-linkedin-profile-data.p.rapidapi.com/get-linkedin-profile-by-salesnavurl"
-=======
 @app.route('/getRecipe')
 def get_recipe():
     
     url = "https://tasty.p.rapidapi.com/recipes/auto-complete"
->>>>>>> 5b0bd5d1705022674796df839111db927ae70a75
 
     querystring = {"prefix":"chicken soup"}
 
@@ -27,15 +21,8 @@ def get_recipe():
     }
 
     response = requests.get(url, headers=headers, params=querystring)
-<<<<<<< HEAD
-    if response.status_code==200:
-        posts_data=response.json()
-    else:
-        posts_data=[]
-=======
 
     posts_data=response.json()
->>>>>>> 5b0bd5d1705022674796df839111db927ae70a75
     if isinstance(posts_data,list):
         posts=posts_data
     elif isinstance(posts_data,dict) and 'results' in posts_data:
@@ -105,6 +92,39 @@ def keto_diet():
         posts=[]
     return render_template('keto_diet.html',posts=posts)
 
+@app.route('/get_recipe',methods=['POST','GET'])
+def get_recipe1():
+    import requests
+
+    url = "https://ai-food-recipe-generator-api-custom-diet-quick-meals.p.rapidapi.com/generate"
+    posts_data=[]
+    posts=[]
+    querystring = {"noqueue":"1"}
+    if request.method=='POST':
+        payload = {
+            "ingredients": ["chicken", "rice", "pepper"],
+            "dietary_restrictions": ["gluten_free"],
+            "cuisine": request.form['cuisine'],
+            "meal_type": request.form['mealtype'],
+            "servings": 4,
+            "lang": "en"
+        }
+        headers = {
+            "x-rapidapi-key": "0744311d70mshf2a21dd15e4df85p10d75ejsn5eff9af59b2a",
+            "x-rapidapi-host": "ai-food-recipe-generator-api-custom-diet-quick-meals.p.rapidapi.com",
+            "Content-Type": "application/json"
+        }
+
+        response = requests.post(url, json=payload, headers=headers, params=querystring)
+        posts_data=response.json()
+        print(posts_data)
+        if isinstance(posts_data,list):
+            posts=posts_data
+        elif isinstance(posts_data,dict) and 'result' in posts_data:
+            posts=[posts_data['result']]
+        else:
+            posts=[]
+    return render_template('get_recipe.html',posts=posts)
 
 
 if __name__=="__main__":
